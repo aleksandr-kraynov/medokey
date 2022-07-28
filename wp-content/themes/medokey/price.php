949678837aad7dfa9deb0e4803e2f3653e1f1910
Mihail Kalugin <mkalugin21@gmail.com>
2022-07-28 14:50:40 +0400

    finished section price

---
+++ wp-content/themes/medokey/price.php
@@ -0,0 +1,208 @@
+<? 
+/*
+Template Name: Стоимость
+*/
+get_header();
+?>
+
+<div class="header-page">
+        <div class="breadcrumb"></div>
+        <div class="page-heading">
+            <h2 class="heading">Стоимость</h2>
+        </div>
+    </div>
+    <section class="price">
+        <div class="price__container">
+            <ul class="price__list">
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1200 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1000 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">600 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1200 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1000 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">600 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1200 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1000 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">600 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1200 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">1000 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div class="card-certificate">
+                        <picture class="card-certificate__picture">
+                            <img class="card-certificate__image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/image163.jpg" alt="" >
+                        </picture>    
+                        <p class="card__short-descriprion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit purus auctor et aliquet proin hac fames. </p>   
+                        <div class="card__price">
+                            <span class="price-value">600 руб</span>
+                        </div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>        
+    </section>
+    <secton class="contacts">
+        <div class="container">
+            <div class="contacts__left">
+                <span class="contacts__title"></span>
+                <span class="contacts__address"></span>
+                <a class="contacts__phone" href="tel:+74994600781">+7 (499) 460-07-81</a>   
+                <div class="contacts__opening-hours">
+                    <ul class="hours__time-list">
+                        <li class="time-list__item"></li>
+                        <li class="time-list__item"></li>
+                        </ul> 
+                </div>
+            </div>
+            <div class="contacts__right">
+                <div class="contacts__map"></div>
+            </div>
+        </div>
+    </secton>
+
+
+<? get_footer();?>
