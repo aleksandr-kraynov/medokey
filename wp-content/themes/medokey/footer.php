212ec4f3c558ceec1fcaca27d117b59ed7e842e5
lllytnik <papa.carlo.link@gmail.com>
2022-07-28 17:03:30 +0400

    add footer

---
+++ wp-content/themes/medokey/footer.php
@@ -0,0 +1,130 @@
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-top">
+                    <div class="footer__logo logo">
+                        <a class="logo__link" href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo.svg'"></a>
+                </div>
+
+                <div class="footer__nav-left nav">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a class="nav__link" href="">Стоимость</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Доставка</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Оплата</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Отзывы</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Статьи</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Оформить заказ</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">О нас</a></li>
+                    </ul>
+                </div>
+                <div class="footer__nav-right nav">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a class="nav__link" href="">Справка 095У</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Справка в вуз</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Справка в школу</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Справка о нетрудоспособности</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Справка о болезни</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Справка для гибдд</a></li>
+                        <li class="nav__item"><a class="nav__link" href="">Другие справки</a></li>
+                    </ul>
+                </div>
+                <div class="footer__info-block">
+                    <div class="call-block">
+                        <a class="tel tel-footer" href="tel:+74994600781">+7 (499) 460-07-81</a>
+                        <button class="call-block__btn call-block__btn-footor">Обратный звонок</button>
+                    </div>
+                    <div class="time-work">
+                        <span>Пн-Пт с 7:00 до 22:00</span>
+                        <span>Сб-Вс с 9:00 до 21:00</span>
+                    </div>
+                    <div class="location">Москва, Зубовский бульвар, 25
+                        (метро Парк Культуры)</div>
+                </div>
+                <div id="accordion" class="accordion">
+                    <div class="accordion__item">
+                        <div class="accordion__header">
+                            Компания
+                        </div>
+                        <div class="accordion__body">
+                            <ul class="nav__list accordion__content">
+                                <li class="nav__item"><a class="nav__link" href="">Стоимость</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Доставка</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Оплата</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Отзывы</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Статьи</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Оформить заказ</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">О нас</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="accordion__item">
+                        <div class="accordion__header">
+                            Каталог
+                        </div>
+                        <div class="accordion__body">
+                            <ul class="nav__list accordion__content">
+                                <li class="nav__item"><a class="nav__link" href="">Справка 095У</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Справка в вуз</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Справка в школу</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Справка о нетрудоспособности</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Справка о болезни</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Справка для гибдд</a></li>
+                                <li class="nav__item"><a class="nav__link" href="">Другие справки</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="accordion__item">
+                        <div class="accordion__header">
+                            Контакты
+                        </div>
+                        <div class="accordion__body">
+                            <div class="accordion__content">
+                                <div class="contact__inner">
+                                    <div class="time-work">
+                                        <span>Время работы</span>
+                                        <span>Пн-Пт с 7:00 до 22:00</span>
+                                        <span>Сб-Вс с 9:00 до 21:00</span>
+                                    </div>
+                                    <div class="location">
+                                        <span>Время работы</span>
+                                        <span>Москва, Зубовский бульвар, 25
+                                            (метро Парк Культуры)</span>
+                                        <a class="location__link" href="">Посмотреть на карте</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="copyright"> Copyright 2022 MED-OK - Медицинские справки, оформление медицинских документов
+                </div>
+                <div class="footer-paymets">
+                    <ul class="paymet__list">
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/mir.svg" alt="mir"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/visa.svg" alt="visa"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/sber.svg" alt="sber"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/mc.svg" alt="mc"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/sber.svg" alt="sber"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/mc.svg" alt="mc"></li>
+                        <li class="paymet__item"><img class="paymet__img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/visa.svg" alt="visa"></li>
+                    </ul>
+                </div>
+                <div class="line"></div>
+                <div class="write-me">
+                    <a class="write-me__link" href="">Напишите&nbsp;нам, мы&nbsp;онлайн!</a>
+                </div>
+            </div>
+
+        </div>
+        </div>
+    </footer>
+    <script>
+        new ItcAccordion(document.querySelector('.accordion'), {
+            alwaysOpen: true
+        });
+    </script>
+</body>
+
+</html>
--- wp-content/themes/medokey/functions.php
+++ wp-content/themes/medokey/functions.php
@@ -1,7 +1,7 @@
 <? 
 
 add_action( 'wp_enqueue_scripts', 'medokey_scripts');
-
+add_action( 'wp_enqueue_scripts', 'true_include_myscript', 25 );
 function medokey_scripts() {
 
     wp_enqueue_style('normalize', get_template_directory_uri() . '/assets/css/normalize.css' );
@@ -14,6 +14,10 @@ function medokey_scripts() {
     wp_enqueue_script('jquery');
     wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 'null', true);
 };
+
+function true_include_myscript() {
+	wp_enqueue_script( 'myscript', get_stylesheet_directory_uri() . '/assets/js/accordion.js', array(), '3.0' );
+}
 
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
