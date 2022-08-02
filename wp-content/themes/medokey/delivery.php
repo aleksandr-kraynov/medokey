bd667958e857e0964679c746f2143cd1983a8d05
Aleksandr <aleksandr_kraynov@hotmail.com>
2022-08-02 09:55:44 +0400

    fix delivery and adapt

--- wp-content/themes/medokey/delivery.php
+++ wp-content/themes/medokey/delivery.php
@@ -5,6 +5,7 @@ Template Name: Доставка
 get_header()
 ?>
 
+
 <div class="header-page">
     <div class="container container--heading">
         <div class="breadcrumb"></div>
@@ -23,8 +24,7 @@ get_header()
             Курьерская служба нашей компании готова доставить ваш документ быстро и
             абсолютно бесплатно по указанному при заказе адресу (в пределах МКАД).
         </div>
-    </div>  
-    
+    </div> 
     <div class="courier-services">            
         <div class="courier-services__item time">            
             <div class="courier-services__body">
@@ -48,9 +48,7 @@ get_header()
                 </div>
             </div>
         </div>
-
     </div>
-
     <div class="delivery-location">
         <p>Экономьте время, выбирая удобную доставку.</p>
         <p>Доставим больничные листы в города МО:</p>
@@ -74,40 +72,8 @@ get_header()
     </div>    
 </div>
 
-    <!-- <div class="container__gelocation">
-        <div class="gelocation">            
-            <div class="contact__items">
-                <div class="contact__item">
-                    <div class="contact__title">
-                        Свяжитесь с нами:
-                    </div>
-                    <div class="contact__address">
-                        Москва, Зубовский бульвар, 25
-                        (метро Парк Культуры)
-                    </div>
-                    <div class="contact__iphone">
-                        <a href="tel:84994600781" class="iphone-contact">+7 (499) 460-07-81</a>
-                    </div>
-                    <div class="contact__work">
-                        <p>Время работы: с 07:00 до 22:00, Пн - Пт</p>
-                        <p>Время работы: с 09:00 до 21:00, Сб - Вс</p>
-                    </div>
-                </div>
-            </div>
-            <div class="gelocation__items">
-                <div class="gelocation__item">
-                    <div style="position:relative;overflow:hidden;"><a
-                            href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps"
-                            style="color:#eee;font-size:12px;position:absolute;top:0px;">Москва</a><a
-                            href="https://yandex.ru/maps/213/moscow/house/zubovskiy_bulvar_25/Z04Ycw9pQE0CQFtvfXtyd3hrZw==/?ll=37.589985%2C55.736483&utm_medium=mapframe&utm_source=maps&z=17.09"
-                            style="color:#eee;font-size:12px;position:absolute;top:14px;">Зубовский бульвар, 25 —
-                            Яндекс Карты</a><iframe src="https://yandex.ru/map-widget/v1/-/CCURUYU3pB" width="100%"
-                            height="654px" frameborder="1" allowfullscreen="true"
-                            style="position:relative;"></iframe></div>
-                </div>
-            </div>
-        </div>
-    </div> -->
+<?php get_template_part('location'); ?>
+   
 
 </div>
 
