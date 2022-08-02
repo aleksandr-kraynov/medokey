b36b1d76a0591ae5298ccece450c185819def6f4
Mihail Kalugin <mkalugin21@gmail.com>
2022-08-02 17:31:17 +0400

    add woocommerce and modified function.php

--- wp-content/themes/medokey/functions.php
+++ wp-content/themes/medokey/functions.php
@@ -33,3 +33,45 @@ function change_menu_item_css_classes( $classes, $item, $args, $depth ) {
 	$classes[] = 'header__nav-item';
 	return $classes;
 }
+//  включаю поддержку woocommerce в теме сайта
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<div class="container">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</div>';
+}
+
+
+remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+
+/*
+** Отключение вкладок на странице товара
+*/
+ 
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+ 
+function woo_remove_product_tabs( $tabs ) {
+ 
+unset( $tabs['reviews'] ); // Убираем вкладку "Отзывы"
+unset( $tabs['additional_information'] ); // Убираем вкладку "Свойства"
+ 
+return $tabs;
+ 
+}
